346ffd2662ed932a788582a8b836f9fdae420c4f
Journey <journesky@gmail.com>
2018-03-07 18:17:47 +0800

    add jsoup topiclistbean

---
+++ app/src/main/java/me/journey/android/v2ex/bean/JsoupTopicListBean.kt
@@ -0,0 +1,14 @@
+package me.journey.android.v2ex.bean
+
+class JsoupTopicListBean {
+    var id: Int = 0
+    var title: String? = null
+    var url: String? = null
+    var replies: Int = 0
+    var member_name: String? = null
+    var member_avatar: String? = null
+    var node: String? = null
+    var last_modified:  String? = null
+}
+
+
--- app/src/main/java/me/journey/android/v2ex/utils/GetListNodeTopicsTask.kt
+++ app/src/main/java/me/journey/android/v2ex/utils/GetListNodeTopicsTask.kt
@@ -2,6 +2,7 @@ package me.journey.android.v2ex.utils
 
 import android.os.AsyncTask
 import com.orhanobut.logger.Logger
+import me.journey.android.v2ex.bean.JsoupTopicListBean
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
@@ -18,27 +19,42 @@ class GetListNodeTopicsTask : AsyncTask<String, Any, Document>() {
     override fun onPostExecute(document: Document?) {
         super.onPostExecute(document)
 //        Logger.d(document?.body().toString())
-        val content = document?.body()?.selectFirst("#Wrapper")
-                ?.selectFirst(".content")
-                ?.selectFirst("#Main")
-                ?.select(".box")
-        val topicList = content!!.select(".item")
+        val content = document!!.body().selectFirst("#Wrapper")
+                .selectFirst(".content")
+                .selectFirst("#Main")
+                .select(".box")
+                .select(".item")
                 .select("table")
                 .select("tbody")
                 .select("tr")
-        for (element in topicList) {
+        val topicList: ArrayList<JsoupTopicListBean> = ArrayList<JsoupTopicListBean>()
+        for (element in content) {
             Logger.d(element.toString())
+            val td = element.select("td")
+            val jsTopicListBean: JsoupTopicListBean = JsoupTopicListBean()
+            jsTopicListBean.member_name = td[0].select("a").attr("href")
+            jsTopicListBean.member_avatar = td[0].select("a").select("img").attr("src")
+            jsTopicListBean.url = td[2].select(".item_title").select("a").attr("href")
+            jsTopicListBean.title = td[2].select(".item_title").select("a").text()
+            jsTopicListBean.node = td[2].select(".fade").select(".node").text()
+            jsTopicListBean.replies = td[3].select("a").text().toInt()
+            var strList: List<String> = td[2].select(".fade").text().split(" • ")
+            if (strList.size > 2) {
+                jsTopicListBean.last_modified = strList[2]
+            } else {
+                jsTopicListBean.last_modified = ""
+            }
+            
+            topicList.add(jsTopicListBean)
+            Logger.d(jsTopicListBean.member_name + "\n" +
+                    jsTopicListBean.member_avatar + "\n" +
+                    jsTopicListBean.title + "\n" +
+                    jsTopicListBean.node + "\n" +
+                    jsTopicListBean.last_modified + "\n" +
+                    jsTopicListBean.replies + "\n" +
+                    jsTopicListBean.url)
         }
 
     }
-
-//    fun parseDocForTab(Document doc){
-//        val elements = doc.select("#Main > div:nth-child(2) > .item  tr")
-//        val result:ArrayList = ArrayList()
-//        for (item in elements) {
-//            result.add(parseItemForTab(item))
-//        }
-//        return result
-//    }
 
 }
