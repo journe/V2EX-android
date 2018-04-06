e3d8a6b9e17f1918defc4d7d063cf933cc46a9b2
Journey <journesky@gmail.com>
2018-04-07 03:37:36 +0800

    show comments correctly

--- app/src/main/java/me/journey/android/v2ex/ui/TopicDetailActivity.kt
+++ app/src/main/java/me/journey/android/v2ex/ui/TopicDetailActivity.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.view.ViewGroup
 import com.zzhoujay.richtext.RichText
 import kotlinx.android.synthetic.main.activity_topic_detail.*
 import me.journey.android.v2ex.R
+import me.journey.android.v2ex.TopicCommentItemAdapter
 import me.journey.android.v2ex.bean.JsoupTopicDetailBean
 import me.journey.android.v2ex.utils.GetTopicDetailTask
 import me.journey.android.v2ex.utils.ImageLoader
@@ -22,12 +25,14 @@ class TopicDetailActivity : AppCompatActivity() {
         val view = this.layoutInflater.inflate(R.layout.activity_topic_detail, null as ViewGroup?, false)
         setContentView(R.layout.activity_topic_detail)
         setSupportActionBar(topic_detail_toolbar)
-
         topicId = intent.extras[TOPIC_ID] as Int
         initView(view)
     }
 
     private fun initView(view: View) {
+        topic_detail_comments_list.layoutManager = LinearLayoutManager(this)
+        topic_detail_comments_list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
         object : GetTopicDetailTask() {
             override fun onStart() {
             }
@@ -44,6 +49,8 @@ class TopicDetailActivity : AppCompatActivity() {
                 topic_detail_menber_name_tv.text = topicDetail.memberBean.username
                 ImageLoader.displayImage(view, topicDetail.memberBean.avatar,
                         topic_detail_avatar, R.mipmap.ic_launcher_round, 4)
+
+                topic_detail_comments_list.adapter = TopicCommentItemAdapter(topicDetail.comments)
             }
 
         }.execute(topicId.toString())
